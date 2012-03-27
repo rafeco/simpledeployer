227e2b3cabb937fbdc37fabd897c3adf1ee321eb
Rafe Colburn <rafeco@gmail.com>
2012-03-27 01:04:12 -0400

    added new config params for deployment scripts

--- config.php
+++ config.php
@@ -1,4 +1,10 @@
 <?php
 
-// The path to the SQLite database
+// path to the SQLite database
 define("DEPLOYMENT_DB_URL", "sqlite:/tmp/deployments.sqlite3");
+
+// path to the deployment scripts
+define("DEPLOYMENT_SCRIPTS", "apps");
+
+// path to SSH keys
+define("DEPLOYMENT_SSH_KEYS", "keys");
