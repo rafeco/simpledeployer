f6fb8ee36b4a7ba557b2159eb652f77e6c183e8e
Rafe Colburn <rafeco@gmail.com>
2012-03-31 11:26:01 -0400

    beautified the deployments list a bit

--- deployments.php
+++ deployments.php
@@ -9,9 +9,9 @@
         <ul>
             <?php foreach ($deployments as $d): ?>
                 <li><?= $d->deployed_by ?> deployed
-                    <a href="deployment.php?id=<?= $d->id ?>"><?= $d->app ?>
-                    (tag <?= $d->deploy_tag ?>)</a>
-                    to <?= $d->environment ?>
+                    <strong><?= $d->app ?></strong>
+                    (<a href="deployment.php?id=<?= $d->id ?>"><?= $d->deploy_tag ?></a>)
+                    to <span class="<?= $d->environment ?>"><?= $d->environment ?></span>
                     on <?= $d->pretty_deployment_date() ?></li>
             <?php endforeach; ?>
         </ul>
